534a53da9c8df65f99a863be0dffd423ac6d80e3
Abin-ms <abinn.mss@gmail.com>
2026-07-09 18:33:34 +0530

    initail commit

---
+++ num_guessing_game.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+class num_guessing_game{
+    public static void main(String []args){
+        Random rand = new Random();
+        Scanner ss = new Scanner(System.in);
+
+        int randomnum = rand.nextInt(101);
+
+        while (true) {
+
+        System.out.println("Enter your guess");
+        int guess = ss.nextInt();
+ 
+        if(randomnum==guess){
+            System.out.println("Congratulations your guess was right");
+            break;
+
+        }
+        else if (randomnum<guess) {
+            System.out.println("Your guess was too high");
+        }  
+        else{
+            System.out.println("your guess was too low");
+        }
+        }
+        ss.close();
+    }
+}
