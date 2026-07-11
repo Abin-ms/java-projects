e78e5c960c6cbc878c99103b0102929d51b8f5dd
Abin-ms <abinn.mss@gmail.com>
2026-07-12 00:19:41 +0530

    commiting rock-paper-scissors

--- rockpaperscissor.java
+++ rockpaperscissor.java
@@ -8,13 +8,17 @@ public class rockpaperscissor {
 
         int computersChoice;
         String user, computer;
+        int score1 = 0;
+        int score2 = 0;
 
         while (true) {
+            System.out.println("____________________\n");
             System.out.println("Enter your choice\n");
             System.out.println("1.Rock\n");
             System.out.println("2.Paper\n");
             System.out.println("3.Scissors\n");
             System.out.println("4.Exit");
+            System.out.println("____________________\n");
 
             int choice = ss.nextInt();
 
@@ -29,57 +33,82 @@ public class rockpaperscissor {
             }
 
             computersChoice = rand.nextInt(3) + 1;
+            user = getUsersChoice(choice);
+            computer = getComputersChoice(computersChoice);
 
-            if (choice == 1 || computersChoice == 1) {
-                user = computer = "Rock";
-            } else if (choice == 2 || computersChoice == 2) {
-                user = computer = "Paper";
-            } else if (choice == 3 || computersChoice == 3) {
-                user = computer = "Scissors";
-            } else {
-                continue;
-            }
+            System.out.println("\nUsers input is : " + user + "\n");
+            System.out.println("Computers input is : " + computer + "\n");
 
-            if (choice == 1 && computersChoice == 2) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("User wins");
-                continue;
-            } else if (choice == 1 && computersChoice == 3) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("User wins");
-                continue;
-            } else if (choice == 2 && computersChoice == 1) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("User wins");
-                continue;
-            } else if (choice == 2 && computersChoice == 3) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("Computer wins");
-                continue;
-            } else if (choice == 3 && computersChoice == 1) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("Computer wins");
-                continue;
-            } else if (choice == 3 && computersChoice == 2) {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
-                System.out.println("User wins");
-                continue;
-            } else {
-                System.out.println("Users input : " + user + "\n");
-                System.out.println("Computers entry : " + computer + "\n");
+            if (user == computer) {
+                 System.out.println("____________________\n");
                 System.out.println("The match is a draw");
-                continue;
+                System.out.println("score : \n");
+                System.out.println("user : " + score1);
+                System.out.println("Computer : " + score2);
+                 System.out.println("____________________\n");
+            } else if (choice == 1 && computersChoice == 3) {
+                 System.out.println("____________________\n");
+                System.out.println("User Wins");
+                score1 = score1 + 1;
+                System.out.println("score : ");
+                System.out.println("user : " + score1);
+                System.out.println("Computer : " + score2);
+                 System.out.println("____________________\n");
+            } else if (choice == 2 && computersChoice == 1) {
+                 System.out.println("____________________\n");
+                System.out.println("User Wins");
+                score1 = score1 + 1;
+                System.out.println("score : ");
+                System.out.println("user : " + score1);
+                System.out.println("Computer : " + score2);
+                 System.out.println("____________________\n");
+            } else if (choice == 3 && computersChoice == 2) {
+                 System.out.println("____________________\n");
+                System.out.println("User Wins");
+                score1 = score1 + 1;
+                System.out.println("score : ");
+                System.out.println("user : " + score1);
+                System.out.println("Computer : " + score2);
+                 System.out.println("____________________\n");
+            } else {
+                 System.out.println("____________________\n");
+                System.out.println("Computer Wins");
+                score2 = score2 + 1;
+                System.out.println("score : ");
+                System.out.println("user : " + score1);
+                System.out.println("Computer : " + score2);
+                 System.out.println("____________________\n");
             }
 
         }
         ss.close();
 
+    }
+
+    public static String getUsersChoice(int choice) {
+        switch (choice) {
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                return "invalid";
+        }
+    }
+
+    public static String getComputersChoice(int computersChoice) {
+        switch (computersChoice) {
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                return "invalid";
+        }
     }
 
 }
