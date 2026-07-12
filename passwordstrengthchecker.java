ffa0d0f5433c735c6617d7694ab8b7e7ab3a0c27
Abin-ms <abinn.mss@gmail.com>
2026-07-12 17:10:39 +0530

    final draft of password checker

--- passwordstrengthchecker.java
+++ passwordstrengthchecker.java
@@ -7,6 +7,7 @@ public class passwordstrengthchecker {
         int lowerCount = 0;
         int numberCount = 0;
         int specialCount = 0;
+        int score = 0;
 
         System.out.println("\n===================\n");
         System.out.println("Enter Your Password");
@@ -18,8 +19,11 @@ public class passwordstrengthchecker {
             ss.close();
             return;
 
+        } else {
+            System.out.println("Entered Password is : " + password);
+
         }
-        System.out.println("Entered Password is : " + password);
+
         for (int i = 0; i < password.length(); i++) {
             char ch = password.charAt(i);
             if (Character.isUpperCase(ch)) {
@@ -30,6 +34,7 @@ public class passwordstrengthchecker {
 
             } else if (Character.isDigit(ch)) {
                 numberCount++;
+
             } else {
                 if (ch == ' ') {
                     System.out.println("The password should not contain Blank spaces");
@@ -37,18 +42,51 @@ public class passwordstrengthchecker {
                     return;
                 } else {
                     specialCount++;
+
                 }
             }
 
         }
-        if (!check(upperCount, lowerCount, numberCount, specialCount)) {
-            System.out.println("The password is not strong");
-            System.out.println("Number of uppercase letters : " + upperCount);
-            System.out.println("Number of lowercase letters : " + lowerCount);
-            System.out.println("Number of digits : " + numberCount);
-            System.out.println("Number of special characters : " + specialCount);
-        } else {
-            System.out.println("The pasword is strong");
+
+        if (password.length() >= 8) {
+            score++;
+        }
+        if (upperCount >= 1) {
+            score++;
+
+        }
+        if (lowerCount >= 1) {
+            score++;
+        }
+        if (numberCount >= 1) {
+            score++;
+        }
+        if (specialCount >= 1) {
+            score++;
+
+        }
+        System.out.println("\n======================================\n");
+        System.out.println("Password Analysis");
+        System.out.println("\n======================================\n");
+        System.out.println("Number of uppercase letters : " + upperCount);
+        System.out.println("Number of lowercase letters : " + lowerCount);
+        System.out.println("Number of digits : " + numberCount);
+        System.out.println("Number of special characters : " + specialCount);
+        System.out.println("strength score is : " + score);
+        System.out.println("\n======================================\n");
+
+        if (score <= 2) {
+
+            System.out.println("Password is Weak");
+        } else if (score == 3) {
+
+            System.out.println("Password is Medium");
+        } else if (score == 4) {
+
+            System.out.println("Password is Good");
+        } else if (score >= 5) {
+
+            System.out.println("Password is Strong");
         }
 
         ss.close();
@@ -57,14 +95,6 @@ public class passwordstrengthchecker {
     public static boolean checkLength(String password) {
         return password.length() >= 8;
 
-    }
-
-    public static boolean check(int upperCount, int lowerCount, int numberCount, int specialCount) {
-        if (upperCount >= 1 && lowerCount >= 1 && numberCount >= 1 && specialCount >= 1) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
