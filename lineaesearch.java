b510909452e2ad802f0c2888907c96712541a757
Abin-ms <abinn.mss@gmail.com>
2026-07-14 22:19:26 +0530

    buhaaa

---
+++ lineaesearch.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class lineaesearch {
+    public static void main(String[] args){
+        Scanner ss = new Scanner(System.in);
+        System.out.println("__________________");
+        System.out.println("Linear search");
+        System.out.println("__________________");
+        
+    }
+}
