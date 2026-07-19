294d3cbd89d5e26d314b47678196fb3e1c85848a
Abin-ms <abinn.mss@gmail.com>
2026-07-19 19:07:13 +0530

    liner searchhh updating

--- lineaesearch.java
+++ lineaesearch.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 public class lineaesearch {
     public static void main(String[] args){
         Scanner ss = new Scanner(System.in);
+       
         System.out.println("__________________");
         System.out.println("Linear search");
         System.out.println("__________________");
-
+        System.out.println("Enter the size of the array");
+        int size = ss.nextInt();
+         int[] arr = new int[size];
         System.out.println("Enter the array");
+        for(int i=0;i<size;i++){
+            arr[i] = ss.nextInt();
+        }
+
+        
         
     }
 }
