ca089a999f0ec9f36e3d072d4c316151502f7da2
Abin-ms <abinn.mss@gmail.com>
2026-07-23 01:14:49 +0530

    pushing the multiplication table generator level - 3

---
+++ multiplicationtablegenerator3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//Multiplication table generator level - 3 (displaying multiplicayion tables of multiple nummbers)
+
+public class multiplicationtablegenerator3 {
+    public static void main(String[] args){
+        Scanner ss = new Scanner(System.in);
+        int range1;
+        int range2;
+        int result;
+        System.out.println("Enter the starting number : ");
+        range1 = ss.nextInt();
+        System.out.println("Enter the ending number : ");
+        range2 = ss.nextInt();
+
+        int i;
+        for(i = range1 ; i<=range2; i++){
+            for(int j = 1 ; j<11 ; j++){
+                result = range1 * j;
+                System.out.println(+range1+"X"+j+"="+result);
+            }
+        }
+        
+
+
+ss.close();
+    }
+}
