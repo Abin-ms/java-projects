8de84e67f9436a29f70c02f6554929423eb28105
Abin-ms <abinn.mss@gmail.com>
2026-07-22 19:59:35 +0530

    pushing atm simulator  project

---
+++ atmsimu.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class atmsimu {
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);        int balance = 1000;
+        int choice;
+
+        while (true) {
+
+            System.out.println("1.Check Balance.");
+            System.out.println("2.Deposit Money");
+            System.out.println("3.Withdraw Money");
+            System.out.println("Exit");
+            
+            choice = ss.nextInt();
+            switch (choice) {
+                case 1:
+                    
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+                    
+                case 4:
+
+                    break;
+            
+                default:
+                    break;
+            }
+
+
+        }
+
+    }
+}
