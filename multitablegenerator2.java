c5271a486142d84c696c25748dbf64bbbba197c8
Abin-ms <abinn.mss@gmail.com>
2026-07-23 00:59:30 +0530

    pushing the multiplication table generator level - 2

--- multitablegenerator2.java
+++ multitablegenerator2.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
-//level - 1 , printing multiplicationtable upto range 10 of a single number.
-public class multitablegenerator {
+//multiplication table level - 2.
+
+public class multitablegenerator2 {
     public static void main(String[] args){
         int result;
         Scanner ss = new Scanner(System.in);
 
         System.out.println("Enter the number : ");
         int number = ss.nextInt();
-        for(int i = 1 ; i<11 ; i++){
-            result = number*1;
 
+        System.out.println("Enter the range");
+        int range = ss.nextInt();
+
+        for(int i = 1 ; i<=range ; i++){
+            result = number * i;
             System.out.println(+number+"X"+i+"="+result);
         }
-ss.close();
+        ss.close();
     }
+
 }
