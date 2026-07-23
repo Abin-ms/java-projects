dc004380e511b4a8dd8fa62c1137d29855abe003
Abin-ms <abinn.mss@gmail.com>
2026-07-23 18:23:09 +0530

    parameter converter

---
+++ converter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class converter {
+    public static void main(String[] args){
+        Scanner ss = new Scanner(System.in);
+        while (true) {
+            
+            
+        }
+    }
+    public static void menu(){
+        System.out.println("Choose your Option");
+        System.out.println("1.Length Converter.");
+        System.out.println("2.Weight Converter");
+        System.out.println("3.Currency Converter");
+    }
+}
