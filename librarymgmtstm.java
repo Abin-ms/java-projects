9f932dd0b704d968c10118a1b93614d57e15041e
Abin-ms <abinn.mss@gmail.com>
2026-08-28 21:32:48 +0530

    saving my streak

--- librarymgmtstm.java
+++ librarymgmtstm.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class librarymgmtstm {
     public static void main(String[] args){
         Scanner ss = new Scanner(System.in);
+    
+        ss.close();
     }
+
 }
