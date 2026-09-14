cab70897de0aa063776305db33ece4b1c1b1ad31
Abin-ms <abinn.mss@gmail.com>
2026-09-14 20:14:37 +0530

    solution for the leetcode problem 'Ransom Notes'

---
+++ leetcode/RansomNote.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        String ransomNote = "aa";
+        String magazine = "aab";
+        System.out.println(canConstruct(ransomNote, magazine));
+
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            int index = magazine.charAt(i) - 'a';
+            count[index]++;
+
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+            count[index]--;
+
+            if (count[index] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
