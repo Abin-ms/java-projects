f35ffd17a5eb8533a01511325cba6434206d30c0
Abin-ms <abinn.mss@gmail.com>
2026-09-17 10:57:32 +0530

    shshs

---
+++ ackkkksss/pg1.java
@@ -0,0 +1,12 @@
+package ackkkksss;
+
+public class pg1 {
+    public static void main(String[] args){
+
+        String b = "abc";
+        int num = 123;
+        b = b + num;  // we can add string and an int value and the '+' will concat and make it as one sigle string.
+        System.out.println(b); // abc123
+
+    }
+}
