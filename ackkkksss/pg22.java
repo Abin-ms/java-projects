52a74e3fefec418b34f4d4cc5f461cf1b413b31f
Abin-ms <abinn.mss@gmail.com>
2026-09-18 09:15:33 +0530

    dumpp

---
+++ ackkkksss/pg22.java
@@ -0,0 +1,13 @@
+package ackkkksss;
+
+public class pg22 {
+    public static void main(String[] agrs){
+        System.out.println(fnnn(10,29));
+    }
+    public static int fnnn(int a , int b){
+        a = a + b;
+        return a;
+
+        }
+ 
+}
