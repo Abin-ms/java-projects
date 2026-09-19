be1c32d7a7a088cf9589d05e01b4bbe0c97d8ae5
Abin M.S <abinn.mss@gmail.com>
2026-09-19 22:52:39 +0530

    Update pg1.java

--- ackkkksss/pg1.java
+++ ackkkksss/pg1.java
@@ -5,6 +5,8 @@ public class pg1 {
 
         String b = "abc";
         int num = 123;
+
+        
         b = b + num;  // we can add string and an int value and the '+' will concat and make it as one sigle string.
         System.out.println(b); // abc123
 
